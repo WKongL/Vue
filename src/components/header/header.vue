ae11c79c9c17302e9aaafaba5e55e14c0964dc06
WKongL <ak47o@qq.com>
2017-10-20 20:41:26 +0800

    frame

---
+++ src/components/header/header.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="header">头部</div>
+</template>
+
+<script type="text/ecmascript-6">
+	export default{};
+</script>
+
+<style rel="stylesheet/stylus" lang="stylus">
+	
+</style>
